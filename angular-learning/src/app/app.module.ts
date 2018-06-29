f71d9b004b4a6e1176173bdbec07bb75c8fa0482
Ciaran Evans <ciaran.evans@students.plymouth.ac.uk>
2018-06-29 13:28:03 +0100

    Add dummy dialog component and fix tests

--- angular-learning/src/app/app.module.ts
+++ angular-learning/src/app/app.module.ts
@@ -2,10 +2,12 @@ import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
 
 import { AppComponent } from './app.component';
+import { DummyDialogComponent } from './dummy-dialog/dummy-dialog.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    DummyDialogComponent
   ],
   imports: [
     BrowserModule
